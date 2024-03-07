0a86d908b7373ce2e0f829bcb656bbbad41bfa64
Dario Di Giacomo <Dario.DiGiacomo@student.pxl.be>
2024-03-07 21:16:42 +0100

    teksten aangepast

--- form.js
+++ form.js
@@ -1,3 +1,17 @@
+// var form = document.getElementById('sheetdb-form');
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   fetch(form.action, {
+//     method: "POST",
+//     body: new FormData(document.getElementById("sheetdb-form")),
+//   }).then(
+//     response => response.json()
+//   ).then((html) => {
+//     // you can put any JS code here
+//     window.location.href = "index.html";
+//   });
+// });
+
 var form = document.getElementById('sheetdb-form');
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -7,7 +21,20 @@ form.addEventListener("submit", e => {
   }).then(
     response => response.json()
   ).then((html) => {
-    // you can put any JS code here
-    alert('success')
+    // Toon de pop-up
+    document.getElementById("customPopup").style.display = "block";
+    document.querySelector(".formulier").style.opacity = "0.5";
+
+    // Wacht 3 seconden voordat je naar de indexpagina gaat
+    setTimeout(function() {
+      window.location.href = "index.html";
+    }, 3000);
   });
+});
+
+// Zorg ervoor dat de pop-up verdwijnt wanneer je van pagina wisselt
+window.addEventListener("beforeunload", function() {
+  document.getElementById("customPopup").style.display = "none";
+  document.querySelector(".formulier").style.opacity = "1";
+
 });
